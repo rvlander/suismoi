6e5c361ada096c245e0d559e0da4a57c50cd76ac
Bastien Barthet <bastien.barthet@gmail.com>
2013-11-29 18:25:46 +0100

    ajout du manifest

--- src/com/babadev/suismoi/SuisMoi.java
+++ src/com/babadev/suismoi/SuisMoi.java
@@ -11,8 +11,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ToggleButton;
 
-import com.example.suismoi.R;
-
 public class SuisMoi extends Activity {
 
 	private ToggleButton startStopServicetoggleButton;
